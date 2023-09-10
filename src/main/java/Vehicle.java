decc90a294f72e1ea391513bca06138976a65059
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-10 18:26:10 +0100

    removed vehicle type from vehicle

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,15 +1,8 @@
 public class Vehicle {
-
-    public String vehicleType;
     public String colour;
 
     public Vehicle(String vehicleType, String colour) {
-        this.vehicleType = vehicleType;
         this.colour = colour;
-    }
-
-    public String getVehicleType() {
-        return vehicleType;
     }
 
     public String getColour() {
